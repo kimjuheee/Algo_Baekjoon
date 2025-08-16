16ca593a8d195f291129f45f00f58d29e5f477d0
김주희 <wngml00118@naver.com>
2025-08-16 15:33:07 +0900

    [level 0] Title: 문자열 섞기, Time: 19.48 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,10 +2,10 @@ class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         for(int i = 0; i < str1.length(); i++){
-            char a = str1.charAt(i);
-            answer += a;
-            char b = str2.charAt(i);
-            answer += b;
+            char c1 = str1.charAt(i);
+            answer += c1;
+            char c2 = str2.charAt(i);
+            answer += c2;
         }
         return answer;
     }
