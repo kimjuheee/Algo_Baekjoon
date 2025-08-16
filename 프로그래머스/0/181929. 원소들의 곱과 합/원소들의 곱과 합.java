23c9747a91b73744402af87bb9be46428f825dda
김주희 <wngml00118@naver.com>
2025-08-16 17:23:27 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,22 +1,14 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        int multi = 1;
         int sum = 0;
-        int square = 1;
+        int multi = 1;
         
         for(int i = 0; i < num_list.length; i++){
-            multi *= num_list[i];
             sum += num_list[i];
-            square = sum*sum;
+            multi *= num_list[i];
         }
-
-        if(multi < square){
-            answer = 1;
-        }else{
-            answer = 0;
-        }
-        
-        return answer;
+        sum = sum*sum;
+        return answer = (multi < sum) ? 1 : 0;
     }
 }
