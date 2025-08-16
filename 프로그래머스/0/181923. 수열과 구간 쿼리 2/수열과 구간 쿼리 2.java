2109e4ab04d95c3c7a57290ea0e80e35435b76de
김주희 <wngml00118@naver.com>
2025-08-16 20:30:39 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.14 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -2,28 +2,27 @@ import java.util.*;
 
 class Solution {
     public ArrayList solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
         
         for(int i = 0; i < queries.length; i++){
             int s = queries[i][0];
             int e = queries[i][1];
             int k = queries[i][2];
             
-            int num = Integer.MAX_VALUE;
+            int min = Integer.MAX_VALUE;
             
             for(int j = s; j <= e; j++){
-                if(k < arr[j]){
-                    num = Math.min(num, arr[j]);
+                if(k < arr[j] && min > arr[j]){
+                    min = arr[j];
                 }
             }
             
-            if(num == Integer.MAX_VALUE){
-                list.add(-1);
+            if(min == Integer.MAX_VALUE){
+                answer.add(-1);
             }else{
-                list.add(num);
+                answer.add(min);
             }
         }
-        return list;
+        return answer;
     }
 }
