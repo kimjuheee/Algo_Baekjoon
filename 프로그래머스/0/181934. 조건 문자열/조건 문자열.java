baafd17bcf2f5bcdde29c291eedeb3ccfd05d0d2
김주희 <wngml00118@naver.com>
2025-08-16 16:51:48 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
         boolean answer = false;
-        int answer_num = 0;
+        int ans = 1;
         if(ineq.equals(">") && eq.equals("=")){
             answer = n >= m;
         }else if(ineq.equals("<") && eq.equals("=")){
@@ -11,12 +11,6 @@ class Solution {
         }else{
             answer = n < m;
         }
-        
-        if(answer == false){
-            answer_num = 0;
-        }else{
-            answer_num = 1;
-        }
-        return answer_num;
+        return ans = (answer ==  true) ? 1 : 0;
     }
 }
