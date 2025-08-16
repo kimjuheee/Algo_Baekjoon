ec89751de47fb062fe0a30005eff3509d1e7032f
김주희 <wngml00118@naver.com>
2025-08-16 19:12:19 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -8,13 +8,11 @@ class Solution {
         for(int i = 0; i < num_list.length; i++){
             answer.add(num_list[i]);
         }
-
-        // 마지막 원소가 그전 원소보다 큰 경우
-        if(num_list[num_list.length-1] > num_list[num_list.length-2]){
-            answer.add(num_list[num_list.length-1] - num_list[num_list.length-2]);
-        // 마지막 원소가 그전 원소보다 크지 않은 경우    
+        
+        if(num_list[num_list.length - 1] > num_list[num_list.length - 2]){
+            answer.add(num_list[num_list.length - 1] - num_list[num_list.length - 2]);
         }else{
-            answer.add(num_list[num_list.length-1]*2);
+            answer.add(num_list[num_list.length - 1]*2);
         }
         return answer;
     }
