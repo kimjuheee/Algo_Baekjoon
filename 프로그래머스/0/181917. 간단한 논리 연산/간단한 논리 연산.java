2dfb0312a0830f681a5e9d27e65600321bb24079
김주희 <wngml00118@naver.com>
2025-08-17 16:42:44 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = false;
+        boolean answer = true;
         boolean x1x2 = x1 || x2;
         boolean x3x4 = x3 || x4;
         answer = x1x2 && x3x4;
