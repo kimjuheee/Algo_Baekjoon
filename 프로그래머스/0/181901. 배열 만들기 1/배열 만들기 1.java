42b9bd33a5f373a896fb53fe575381e27972eb5c
김주희 <wngml00118@naver.com>
2025-08-19 21:39:52 +0900

    [level 0] Title: 배열 만들기 1, Time: 11.30 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int n, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++){
+            if(i % k == 0){
+                list.add(i);
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
