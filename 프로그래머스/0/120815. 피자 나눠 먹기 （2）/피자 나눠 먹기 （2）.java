f0fc5db6e55b9f9cff5f924fa9ae6b4fd6786014
김주희 <wngml00118@naver.com>
2025-08-20 17:33:13 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i = 1;
+        while(6*i % n != 0){
+            i++;
+            answer = i;
+        }
+        answer = i;
+        return answer;
+    }
+}
