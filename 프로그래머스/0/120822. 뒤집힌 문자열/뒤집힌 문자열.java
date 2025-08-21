c5d45775a8f40c9ed708dc4b9d924aee676275fb
김주희 <wngml00118@naver.com>
2025-08-21 17:34:49 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(my_string);
+        return answer = sb.reverse().toString();
+    }
+}
