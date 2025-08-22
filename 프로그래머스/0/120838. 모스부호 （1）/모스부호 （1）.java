c4144acc786a8d6fab54c693e7c91479d77e6040
김주희 <wngml00118@naver.com>
2025-08-22 17:51:23 +0900

    [level 0] Title: 모스부호 (1), Time: 2.50 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String[] str = letter.split(" ");
+        for(int i = 0; i < str.length; i++){
+            if(str[i].equals(".-")){
+                answer += "a";
+            }else if(str[i].equals("-...")){
+                answer += "b";
+            }else if(str[i].equals("-.-.")){
+                answer += "c";
+            }else if(str[i].equals("-..")){
+                answer += "d";
+            }else if(str[i].equals(".")){
+                answer += "e";
+            }else if(str[i].equals("..-.")){
+                answer += "f";
+            }else if(str[i].equals("--.")){
+                answer += "g";
+            }else if(str[i].equals("....")){
+                answer += "h";
+            }else if(str[i].equals("..")){
+                answer += "i";
+            }else if(str[i].equals(".---")){
+                answer += "j";
+            }else if(str[i].equals("-.-")){
+                answer += "k";
+            }else if(str[i].equals(".-..")){
+                answer += "l";
+            }else if(str[i].equals("--")){
+                answer += "m";
+            }else if(str[i].equals("-.")){
+                answer += "n";
+            }else if(str[i].equals("---")){
+                answer += "o";
+            }else if(str[i].equals(".--.")){
+                answer += "p";
+            }else if(str[i].equals("--.-")){
+                answer += "q";
+            }else if(str[i].equals(".-.")){
+                answer += "r";
+            }else if(str[i].equals("...")){
+                answer += "s";
+            }else if(str[i].equals("-")){
+                answer += "t";
+            }else if(str[i].equals("..-")){
+                answer += "u";
+            }else if(str[i].equals("...-")){
+                answer += "v";
+            }else if(str[i].equals(".--")){
+                answer += "w";
+            }else if(str[i].equals("-..-")){
+                answer += "x";
+            }else if(str[i].equals("-.--")){
+                answer += "y";
+            }else if(str[i].equals("--..")){
+                answer += "z";
+            }
+        }
+        return answer;
+    }
+}
