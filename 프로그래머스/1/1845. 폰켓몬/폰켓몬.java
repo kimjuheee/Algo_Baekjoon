8710cfe26d2f93efc34a380077b8f28af18ef933
김주희 <wngml00118@naver.com>
2025-10-31 22:55:14 +0900

    [level 1] Title: 폰켓몬, Time: 4.10 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : nums){
+            set.add(num);
+        }
+        
+        int maxSelect = nums.length / 2;
+        int typeCount = set.size();
+        
+        return Math.min(maxSelect, typeCount);
+    }
+}
